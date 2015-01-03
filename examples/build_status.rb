f49e8cf34ad9052df3195f4d0efd1bdb1a80d44e
Brian Kelly <polymonic@gmail.com>
2015-01-02 21:41:50 -0500

    added a simple example

---
+++ examples/build_status.rb
@@ -0,0 +1,65 @@
+require 'l8'
+require 'rest_client'
+require 'json'
+
+def pending(index)
+  @l8.set_led(0, index, 3, 3, 0)
+end
+
+def success(index)
+  @l8.set_led(0, index, 0, 15, 0)
+end
+
+def failure(index)
+  @l8.set_led(0, index, 15, 0, 0)
+  @l8.set_superled(15,0,0)
+end
+
+def fill_timer
+  (0..7).each { |i| @l8.set_led(7, i, 1, 1, 1) }
+end
+
+def waiting_progress
+  (0..7).each do |i|
+    sleep 8
+    @l8.set_led(7, 7 - i, 0, 0, 0)
+  end
+end
+
+def get_status(url)
+  response = RestClient.get url
+  json = JSON.parse(response)
+  json[0]['result']
+end
+
+@l8 = L8::Smartlight.new('/dev/tty.usbmodem1421')
+
+@projects = %w(
+  https://api.travis-ci.org/repos/pivotal/pivotal-life/builds
+  https://api.travis-ci.org/repos/spilth/little_bits/builds
+  https://api.travis-ci.org/repositories/rdkit/rdkit/builds
+  https://api.travis-ci.org/repos/spilth/wedderdotcom/builds
+  https://api.travis-ci.org/repos/spilth/ubyray/builds
+  https://api.travis-ci.org/repos/spilth/littlebits-travis-webhook/builds
+)
+
+@l8.clear_matrix
+
+while true
+  @l8.set_superled(0,15,0)
+  @projects.each_with_index do |url, index|
+    pending(index)
+
+    if get_status(url) == 0
+      success(index)
+    else
+      failure(index)
+    end
+  end
+
+  fill_timer
+
+  waiting_progress
+end
+
+
---
+++ examples/draw.rb
@@ -0,0 +1,59 @@
+require 'l8'
+require 'io/console'
+
+@l8 = L8::Smartlight.new("/dev/tty.usbmodemfa1331")
+
+@x = 0
+@y = 0
+
+def read_char
+  STDIN.echo = false
+  STDIN.raw!
+
+  input = STDIN.getc.chr
+  if input == "\e" then
+    input << STDIN.read_nonblock(3) rescue nil
+    input << STDIN.read_nonblock(2) rescue nil
+  end
+ensure
+  STDIN.echo = true
+  STDIN.cooked!
+
+  return input
+end
+
+def show_single_key
+  c = read_char
+
+  @l8.set_led(@x,@y, 15,15,15)
+
+  case c
+    when " "
+      puts "SPACE"
+    when "\e[A"
+      # up
+      @x = @x - 1
+    when "\e[B"
+      # up
+      @x = @x + 1
+    when "\e[C"
+      # right
+      @y = @y + 1
+    when "\e[D"
+      # left
+      @y = @y - 1
+    when "\u0003"
+      puts "CONTROL-C"
+      exit 0
+    else
+      puts "SOMETHING ELSE: #{c.inspect}"
+  end
+
+  @l8.set_led(@x,@y, 0,15,0)
+end
+
+@l8.clear_matrix
+
+@l8.set_led(@x,@y, 0,15,0)
+
+show_single_key while(true)
