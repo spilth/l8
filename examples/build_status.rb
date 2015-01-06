724f351a51a1452f087e752bdc987fd344c80f9a
Brian Kelly <polymonic@gmail.com>
2015-01-05 22:42:35 -0500

    Even more awesome build statusness

--- examples/build_status.rb
+++ examples/build_status.rb
@@ -2,21 +2,29 @@ require 'l8'
 require 'rest_client'
 require 'json'
 
-def pending(index)
-  @l8.set_led(0, index, 3, 3, 0)
+def pending(column, row)
+  @l8.set_led(row, column, 15, 15, 15)
 end
 
-def success(index)
-  @l8.set_led(0, index, 0, 15, 0)
+def success(column, row)
+  if row == 0
+    @l8.set_led(row, column, 0, 15, 0)
+  else
+    @l8.set_led(row, column, 0, 1, 0)
+  end
 end
 
-def failure(index)
-  @l8.set_led(0, index, 15, 0, 0)
-  @l8.set_superled(15,0,0)
+def failure(column, row)
+  if row  == 0
+    @l8.set_led(row, column, 15, 0, 0)
+    @l8.set_superled(15,0,0)
+  else
+    @l8.set_led(row, column, 1, 0, 0)
+  end
 end
 
 def fill_timer
-  (0..7).each { |i| @l8.set_led(7, i, 1, 1, 1) }
+  (0..7).each { |i| @l8.set_led(7, i, 0, 0, 1 + (i * 2)) }
 end
 
 def waiting_progress
@@ -26,15 +34,15 @@ def waiting_progress
   end
 end
 
-def get_status(url)
+def get_statuses(url)
   response = RestClient.get url
-  json = JSON.parse(response)
-  json[0]['result']
+  JSON.parse(response)
 end
 
 @l8 = L8::Smartlight.new('/dev/tty.usbmodem1411')
 
 @projects = %w(
+  https://api.travis-ci.org/repos/spilth/l8/builds
   https://api.travis-ci.org/repos/pivotal/pivotal-life/builds
   https://api.travis-ci.org/repos/spilth/little_bits/builds
   https://api.travis-ci.org/repositories/rdkit/rdkit/builds
@@ -45,16 +53,31 @@ end
 )
 
 @l8.clear_matrix
+@l8.disable_status_leds
+@l8.set_brightness(:high)
 
 while true
   @l8.set_superled(0,15,0)
-  @projects.each_with_index do |url, index|
-    pending(index)
 
-    if get_status(url) == 0
-      success(index)
-    else
-      failure(index)
+  @projects.each_with_index do |url, column|
+    builds = get_statuses(url)
+
+    builds.each_with_index do |build, row|
+      if row == 7
+        break
+      end
+
+      pending(column, row)
+      sleep 0.1
+
+      status = build['result']
+
+      if status == 0
+        success(column, row)
+      else
+        failure(column, row)
+      end
+
     end
   end
 
