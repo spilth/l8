d31d1481f75226c54106269e8e12c5e16150a549
Brian Kelly <polymonic@gmail.com>
2015-01-10 22:54:55 -0500

    Clean up set_led spec

--- spec/lib/smartlight_spec.rb
+++ spec/lib/smartlight_spec.rb
@@ -9,6 +9,7 @@ module L8
       allow(Kernel).to receive(:at_exit) do |&block|
         block.call
       end
+      allow(serial_port).to receive(:write).with('foo')
       allow(serial_port).to receive(:close)
     end
 
@@ -22,8 +23,7 @@ module L8
 
     describe '#set_led' do
       it 'sets the color of the LED at given location' do
-        allow(Util).to receive(:frame).with([Smartlight::CMD_L8_LED_SET, 3, 0, 15, 15, 15, 0]) { 'foo'}
-        allow(serial_port).to receive(:write).with('foo')
+        expect(Util).to receive(:frame).with([Smartlight::CMD_L8_LED_SET, 3, 0, 15, 15, 15, 0]) { 'foo'}
 
         l8 = L8::Smartlight.new('serial_port')
         l8.set_led(3, 0, 15, 15, 15)
